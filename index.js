d4a0660b19e1da9900f2e4ff36be1c075b8e9d0c
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-11-23 02:29:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ import bodyParser from 'body-parser';
 import { createReadStream } from 'fs';
 import crypto from 'crypto';
 import http from 'http';
-
+import m from 'mongoose';
 import appSrc from './app.js';
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+const UserSchema = new m.Schema({
+    login: {
+      type: 'String'
+    },
+    password: {
+      type: 'String'
+    }
+});
 
-app.listen(process.env.PORT ?? 4321);
+
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, m, UserSchema);
+
+try {
+    await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    app.listen(process.env.PORT ?? 4321);
+} catch(e) {
+    console.log(e.codeName);
+}
